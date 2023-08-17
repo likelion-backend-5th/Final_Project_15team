2a75371aa6ea2fab8c258aa149a21e1aaeb66294
hj-keem <hyeomjeom@gmail.com>
2023-08-17 13:35:41 +0900

    init: first commit

---
+++ src/main/java/com/example/Final_Project_mutso/FinalProjectMutsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Final_Project_mutso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectMutsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectMutsoApplication.class, args);
+	}
+
+}
