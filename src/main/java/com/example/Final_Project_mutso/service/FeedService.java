3213cc509f733f881cf2ed349e2658dd2643d597
Ms Kim <myungseokim9912@gmail.com>
2023-08-23 16:20:26 +0900

    피드 사진 업로드

--- src/main/java/com/example/Final_Project_mutso/service/FeedService.java
+++ src/main/java/com/example/Final_Project_mutso/service/FeedService.java
@@ -2,10 +2,13 @@ package com.example.Final_Project_mutso.service;
 
 import com.example.Final_Project_mutso.dto.FeedDto;
 import com.example.Final_Project_mutso.entity.Feed;
+import com.example.Final_Project_mutso.entity.FeedImage;
+import com.example.Final_Project_mutso.repository.FeedImageRepository;
 import com.example.Final_Project_mutso.repository.FeedRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,13 +20,22 @@ import java.util.Optional;
 public class FeedService {
 
     private final FeedRepository feedRepository;
+    private final FileService fileService;
+    private final FeedImageRepository feedImageRepository;
 
-    public void createFeed(FeedDto dto) {
+    public void createFeed(FeedDto dto, MultipartFile file) {
         Feed feed = new Feed();
 //        feed.setUser(userEntity);
         feed.setTitle(dto.getTitle());
         feed.setContent(dto.getContent());
         feedRepository.save(feed);
+        if (!file.isEmpty()) { // 첨부 파일이 존재한다면
+            String url = fileService.createFile(file);
+            FeedImage feedImage = new FeedImage();
+            feedImage.setFeed(feed);
+            feedImage.setImageUrl(url);
+            feedImageRepository.save(feedImage);
+        }
 
     }
 
