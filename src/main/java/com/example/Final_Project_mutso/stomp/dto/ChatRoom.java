dc922f2865fc892931cf8d5fde1b30020acfe05b
hj-keem <hyeomjeom@gmail.com>
2023-08-25 18:31:26 +0900

    error : conflict 안 뜨도록 수정 (rebase)

--- src/main/java/com/example/Final_Project_mutso/stomp/dto/ChatRoom.java
+++ src/main/java/com/example/Final_Project_mutso/stomp/dto/ChatRoom.java
@@ -1,6 +1,5 @@
 package com.example.Final_Project_mutso.stomp.dto;
 
-//import com.example.Final_Project_mutso.stomp.jpa.ChatRoomEntity;
 import com.example.Final_Project_mutso.stomp.jpa.ChatRoomEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
