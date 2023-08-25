ecd8b68d6072583f2d8e3a08867374492222f554
hj-keem <hyeomjeom@gmail.com>
2023-08-25 18:06:44 +0900

    error : conflict 안 뜨도록 수정 (rebase)

---
+++ src/main/java/com/example/Final_Project_mutso/stomp/repository/ChatRoomRepository.java
@@ -0,0 +1,5 @@
+package com.example.Final_Project_mutso.stomp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChattingRoom, Long> { }
