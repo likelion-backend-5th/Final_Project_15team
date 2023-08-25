0d537a5e1a017ebc2719b1fa4f0fa6f988b62642
hj-keem <hyeomjeom@gmail.com>
2023-08-25 18:40:14 +0900

    error : conflict 안 뜨도록 수정 (rebase)

--- src/main/java/com/example/Final_Project_mutso/stomp/dto/ChatRoom.java
+++ src/main/java/com/example/Final_Project_mutso/stomp/dto/ChatRoom.java
@@ -1,6 +1,6 @@
 package com.example.Final_Project_mutso.stomp.dto;
 
-import com.example.Final_Project_mutso.stomp.jpa.ChatRoomEntity;
+import com.example.Final_Project_mutso.stomp.jpa.ChattingRoom;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +12,7 @@ public class ChatRoom {
     private Long id;
     private String roomName;
 
-    public static ChatRoom fromEntity(ChatRoomEntity entity) {
+    public static ChatRoom fromEntity(ChattingRoom entity) {
         return new ChatRoom(
                 entity.getId(),
                 entity.getRoomName()
