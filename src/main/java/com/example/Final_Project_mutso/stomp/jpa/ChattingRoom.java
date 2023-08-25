4ef00f92adc0538f90e489c881462a718854ff5d
hj-keem <hyeomjeom@gmail.com>
2023-08-25 18:01:34 +0900

    error : conflict 안 뜨도록 수정 (rebase)

--- src/main/java/com/example/Final_Project_mutso/stomp/jpa/ChattingRoom.java
+++ src/main/java/com/example/Final_Project_mutso/stomp/jpa/ChattingRoom.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-public class ChatRoomEntity {
+public class ChattingRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
