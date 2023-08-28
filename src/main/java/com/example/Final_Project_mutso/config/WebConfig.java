5a1df3409c296a4d43b3821a6129935c68489185
hj-keem <hyeomjeom@gmail.com>
2023-08-28 09:31:09 +0900

    fix : WebConfig 파일 Bean name 중복 에러 수정

--- src/main/java/com/example/Final_Project_mutso/config/WebConfig.java
+++ src/main/java/com/example/Final_Project_mutso/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.Final_Project_mutso.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,5 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry){
         registry.addMapping("/**");
+    }
+
+    // js 파일 연결
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
     }
 }
