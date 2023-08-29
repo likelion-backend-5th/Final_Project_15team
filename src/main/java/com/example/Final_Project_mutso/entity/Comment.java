be3231a0f78e1e4dc97cf9abe32999d935c23755
Ms Kim <myungseokim9912@gmail.com>
2023-08-29 11:25:19 +0900

    댓글 등록시 피드 불러오기 오류 수정

--- src/main/java/com/example/Final_Project_mutso/entity/Comment.java
+++ src/main/java/com/example/Final_Project_mutso/entity/Comment.java
@@ -24,7 +24,4 @@ public class Comment {
 
     private String time;
 
-    public Comment() {
-
-    }
 }
--- src/main/java/com/example/Final_Project_mutso/entity/Feed.java
+++ src/main/java/com/example/Final_Project_mutso/entity/Feed.java
@@ -34,7 +34,5 @@ public class Feed {
     @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL)
 //    private List<Comment> comments = new ArrayList<>();
     private List<Comment> comments;
-    public Feed() {
 
-    }
 }
