3482b967dd649fdeb555092791009cb856392c04
zxfg223 <zxfg223@gmail.com>
2023-08-30 14:45:41 +0900

    Remove : 플레이 리스트 공유하는 피드 주석처리

--- src/main/java/com/example/Final_Project_mutso/entity/Likes.java
+++ src/main/java/com/example/Final_Project_mutso/entity/Likes.java
@@ -1,27 +1,27 @@
-package com.example.Final_Project_mutso.entity;
-
-import jakarta.persistence.*;
-import lombok.Data;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
-@Data
-@Entity
-public class Likes {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name="feedId")
-    @ManyToOne
-    private PlayListShareFeed feed;
-
-
-    @JoinColumn(name="userId")
-    @ManyToOne
-    private UserEntity user;
-
-
-}
+//package com.example.Final_Project_mutso.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//import org.hibernate.annotations.OnDelete;
+//import org.hibernate.annotations.OnDeleteAction;
+//
+//@Data
+//@Entity
+//public class Likes {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    @JoinColumn(name="feedId")
+//    @ManyToOne
+//    private PlayListShareFeed feed;
+//
+//
+//    @JoinColumn(name="userId")
+//    @ManyToOne
+//    private UserEntity user;
+//
+//
+//}
