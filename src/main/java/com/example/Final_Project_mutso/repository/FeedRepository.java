095350491bec524c6e9e515e638d781667ec57f6
Ms Kim <myungseokim9912@gmail.com>
2023-09-05 18:05:21 +0900

    좋아요 기능 ing

--- src/main/java/com/example/Final_Project_mutso/repository/FeedRepository.java
+++ src/main/java/com/example/Final_Project_mutso/repository/FeedRepository.java
@@ -10,7 +10,7 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
     @Override
     Optional<Feed> findById(Long id);
 
-    Feed findByFeedId(Long id);
+//    Feed findByFeedId(Long id);
 
 //    Page<Feed> findAllByTitleContains(
 //            String title, Pageable pageable);
