6d2ccffdbf6ece40a5e90da3292140f5e29f9127
hj-keem <hyeomjeom@gmail.com>
2023-09-11 14:39:49 +0900

    fix : security cors 설정

--- src/main/java/com/example/Final_Project_mutso/config/WebConfig.java
+++ src/main/java/com/example/Final_Project_mutso/config/WebConfig.java
@@ -9,16 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080/", "http://localhost:3000") // 허용할 origin을 설정합니다.
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드를 설정합니다.
-                .allowedHeaders("Authorization") // 허용할 요청 헤더를 설정합니다.
+        registry.addMapping("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000") // 허용할 origin을 설정합니다.
+                .allowedMethods("POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드를 설정합니다.
+                .allowedHeaders("*") // 허용할 요청 헤더를 설정합니다.
                 .allowCredentials(true); // 인증 정보 허용 여부를 설정합니다.
     }
 
     // js 파일 연결
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+//    }
 }
