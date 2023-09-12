67eaeb98566bc1da431978a0772b72e35f1b33fa
Ms Kim <myungseokim9912@gmail.com>
2023-09-12 16:50:43 +0900

    엑박 오류 해결

--- src/main/java/com/example/Final_Project_mutso/service/FileService.java
+++ src/main/java/com/example/Final_Project_mutso/service/FileService.java
@@ -29,33 +29,9 @@ public class FileService {
     private final FeedVideoRepository feedVideoRepository;
     private final FeedRepository feedRepository;
 
-    public void identFile(MultipartFile file, Feed feed) throws IOException {
-        if (!file.isEmpty()) { // 첨부 파일이 존재한다면
-            String fileName = file.getOriginalFilename(); //파일명을 얻어낼 수 있는 메서드!
-            String fileExtension = fileName.substring(fileName.lastIndexOf("."),fileName.length());
-
-            if (fileExtension.equals(".jpg")||fileExtension.equals(".png")){// 확장자가 이미지일 때
-                String url = createFile(file);
-                FeedImage feedImage = new FeedImage();
-                feedImage.setFeed(feed);
-                feedImage.setImageUrl(url);
-                feedImageRepository.save(feedImage);
-            } else if (fileExtension.equals(".mp4")||fileExtension.equals(".avi")){ //영상 확장자일 때
-                String url = createFile(file);
-                FeedVideo feedVideo = new FeedVideo();
-                feedVideo.setFeed(feed);
-                feedVideo.setVideoUrl(url);
-                feedVideoRepository.save(feedVideo);
-            }
-            else{
-                System.out.println(".jpg, .png, .mp4, .avi 확장자 파일을 선택해주세요");
-            }
-        }
-    }
-
     public String createFile(MultipartFile file) throws IOException {
 
-        String fileDir = "files/";
+        String fileDir = "back/feed/";
         try {
             Files.createDirectories(Path.of(fileDir));
         } catch (IOException e) {
@@ -67,7 +43,7 @@ public class FileService {
 
         // 이미지 URL 생성
         String fileUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("static/message/") // 이미지 업로드 경로
+                .path("static/feed/") // 이미지 업로드 경로
                 .path(fileName)
                 .toUriString();
 
